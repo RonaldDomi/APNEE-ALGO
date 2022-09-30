3fbbcd9d91ac838a860af3d64bcc0efc006a0bb2
zeynel95 <zeynelaltun95@gmail.com>
2022-09-30 12:41:58 +0200

    modulation2

--- APNEE2/generation.c
+++ APNEE2/generation.c
@@ -2,7 +2,62 @@
 #include <stdio.h>
 #include "tableau.h"
 
-void parenthese_correct(FILE* file, int nbr_min){
+void parentheseValide(FILE* file, int nbr_min){
+    srand(time(NULL));
+    tableau t;
+    init_tab(&t);
+    char car, tmp;
+    int rd, rd2, taille_elem = 0;
+    while (!est_vide(t) || taille_elem < nbr_min)
+    {
+        if(!est_vide(t)) rd = rand() % 2;
+        //si la pile est vide on peut pas avoir de parenthése 
+        //fermante donc on force parenthese ouvrante
+        else rd = 0;
+        switch (rd)
+        {
+        case 0:
+        /*cas parenthese ouvrante*/
+            rd2 = rand()%3;
+            switch (rd2)
+            {
+            case 0:
+                car = '(';
+                break;
+            case 1:
+                car = '[';
+                break;
+            case 2:
+                car = '{';
+                break;    
+            }
+            putc(car, file);
+            empiler(&t, car);
+            break;
+        case 1:
+            /* code parenthese fermante*/
+            tmp = depiler(&t);
+            switch(tmp)
+            {
+                case '(':
+                    car = ')';
+                    break;
+                case '{':
+                    car = '}';
+                    break;
+                case '[':
+                    car = ']';
+                    break;
+            }
+            putc(car, file);
+            break; 
+        }
+        taille_elem++;
+    }
+    putc('\n', file);
+}
+
+void parentheseMauvais(FILE* file, int nbr_min){
     srand(time(NULL));
     tableau t;
     init_tab(&t);
@@ -58,15 +113,23 @@ void parenthese_correct(FILE* file, int nbr_min){
 }
 
 int main(int argc, char **argv){
-    if (argc != 3)
+    if (argc != 4)
     {
         printf("\nNombre dargument incorrect\n");
-        printf("./generation file nbr_min_element\n");
+        printf("./generation fonction file nbr_min_element\n");
         return 1;
     }
-    FILE* file = fopen(argv[1], "w");
-    int nbr_min = atoi(argv[2]);
-    parenthese_correct(file, nbr_min);
+
+    FILE* file = fopen(argv[2], "w");
+    int nbr_min = atoi(argv[3]);
+    if (!strcmp(argv[1], "Valide"))
+    {
+        parentheseValide(file, nbr_min);        
+    }
+    if (!strcmp(argv[1], "Mauvais"))
+    {
+        parentheseMauvais(file, nbr_min);        
+    }
     fclose(file);
     return 0;
 }
