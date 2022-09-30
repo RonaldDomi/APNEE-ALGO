2a39c596ea3cf71eabafdbfc9d6b7615bb052b4f
zeynel95 <zeynelaltun95@gmail.com>
2022-09-30 13:16:57 +0200

    test

--- APNEE2/generation.c
+++ APNEE2/generation.c
@@ -59,16 +59,11 @@ void parentheseValide(FILE* file, int nbr_min){
 
 void parentheseMauvais(FILE* file, int nbr_min){
     srand(time(NULL));
-    tableau t;
-    init_tab(&t);
     char car, tmp;
     int rd, rd2, taille_elem = 0;
-    while (!est_vide(t) || taille_elem < nbr_min)
+    while (taille_elem < nbr_min)
     {
-        if(!est_vide(t)) rd = rand() % 2;
-        //si la pile est vide on peut pas avoir de parenthése 
-        //fermante donc on force parenthese ouvrante
-        else rd = 0;
+        rd = rand() % 2;
         switch (rd)
         {
         case 0:
@@ -87,22 +82,21 @@ void parentheseMauvais(FILE* file, int nbr_min){
                 break;    
             }
             putc(car, file);
-            empiler(&t, car);
             break;
         case 1:
             /* code parenthese fermante*/
-            tmp = depiler(&t);
-            switch(tmp)
+            rd2 = rand()%3;
+            switch (rd2)
             {
-                case '(':
-                    car = ')';
-                    break;
-                case '{':
-                    car = '}';
-                    break;
-                case '[':
-                    car = ']';
-                    break;
+            case 0:
+                car = ')';
+                break;
+            case 1:
+                car = ']';
+                break;
+            case 2:
+                car = '}';
+                break;    
             }
             putc(car, file);
             break; 
@@ -130,6 +124,7 @@ int main(int argc, char **argv){
     {
         parentheseMauvais(file, nbr_min);        
     }
+    else printf("choisir entre Valide et Mauvais");
     fclose(file);
     return 0;
 }
